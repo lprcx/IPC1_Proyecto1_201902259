969a3d3b98733f17571ee9c5ca6cf110c93d6e51
lprcx <3506998280101@ingenieria.usac.edu.gt>
2022-03-06 23:56:12 -0600

    Sufrí poniendo la fecha y las graficas jeje pero pues

--- [IPC1] Proyecto 1_201902259/src/Interfazg/PestañaGraficos.java
+++ [IPC1] Proyecto 1_201902259/src/Interfazg/PestañaGraficos.java
@@ -4,38 +4,82 @@
  */
 package Interfazg;
 
+import ipc1.proyecto.pkg1_201902259.IPC1Proyecto1_201902259;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
-
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 public class PestañaGraficos extends JPanel implements ActionListener {
-    JButton h;
-    
+
+
     public PestañaGraficos() {
+
+        DefaultPieDataset datos1 = new DefaultPieDataset();
+        datos1.setValue("Usuarios Administradores", IPC1Proyecto1_201902259.ust1());
+        datos1.setValue("Usuarios Estudiantes", IPC1Proyecto1_201902259.ust2());
+        JFreeChart graficaus = ChartFactory.createPieChart3D("Tipo de Usuarios Registrados en el Sistema", datos1);
+        PiePlot3D pie1 = (PiePlot3D) graficaus.getPlot();
+        pie1.setForegroundAlpha(0.40f);
+        pie1.setInteriorGap(0.05);
+        pie1.setBackgroundPaint(Color.LIGHT_GRAY);
+        ChartPanel p1 = new ChartPanel(graficaus);
+        p1.setBounds(20, 50, 290, 290);
+        this.add(p1);
+
         
-        //Boton de prueba xd
-        h = new JButton("Libros");
-        h.setBounds(25,80,75,20);
-        h.setFont(new Font("Biome Light", Font.BOLD, 12));
-        h.setBackground(Color.lightGray);
-        this.add(h);
-        
-        
-        
+        DefaultCategoryDataset datos2 = new DefaultCategoryDataset();
+        datos2.setValue(IPC1Proyecto1_201902259.enero(), "Enero", String.valueOf(IPC1Proyecto1_201902259.enero()));
+        datos2.setValue(IPC1Proyecto1_201902259.febrero(), "Febrero", String.valueOf(IPC1Proyecto1_201902259.febrero()));
+        datos2.setValue(IPC1Proyecto1_201902259.marzo(), "Marzo", String.valueOf(IPC1Proyecto1_201902259.marzo()));
+        datos2.setValue(IPC1Proyecto1_201902259.abril(), "Abril", String.valueOf(IPC1Proyecto1_201902259.abril()));
+        datos2.setValue(IPC1Proyecto1_201902259.mayo(), "Mayo", String.valueOf(IPC1Proyecto1_201902259.mayo()));
+        datos2.setValue(IPC1Proyecto1_201902259.junio(), "Junio", String.valueOf(IPC1Proyecto1_201902259.junio()));
+        datos2.setValue(IPC1Proyecto1_201902259.julio(), "Julio", String.valueOf(IPC1Proyecto1_201902259.julio()));
+        datos2.setValue(IPC1Proyecto1_201902259.agosto(), "Agosto", String.valueOf(IPC1Proyecto1_201902259.agosto()));
+        datos2.setValue(IPC1Proyecto1_201902259.septiembre(), "Septiembre", String.valueOf(IPC1Proyecto1_201902259.septiembre()));
+        datos2.setValue(IPC1Proyecto1_201902259.octubre(), "Octubre", String.valueOf(IPC1Proyecto1_201902259.octubre()));
+        datos2.setValue(IPC1Proyecto1_201902259.noviembre(), "Noviembre", String.valueOf(IPC1Proyecto1_201902259.noviembre()));
+        datos2.setValue(IPC1Proyecto1_201902259.diciembre(), "Diciembre", String.valueOf(IPC1Proyecto1_201902259.diciembre()));
+        JFreeChart barras = ChartFactory.createBarChart("Cantidad de Libros Prestados por Fecha", "mes", "cantidad de libros", datos2, PlotOrientation.VERTICAL, true, false, false);
+        barras.setBackgroundPaint(Color.PINK);
+        barras.getTitle().setPaint(Color.BLACK);
+        barras.getTitle().setFont(new Font ("Century Gothic", Font.PLAIN, 15));
+        ChartPanel p2 = new ChartPanel(barras);
+        p2.setBounds(330, 50, 290, 290);
+        this.add(p2);
+
+
+        DefaultPieDataset datos3 = new DefaultPieDataset();
+        datos3.setValue("Libro", IPC1Proyecto1_201902259.tl1());
+        datos3.setValue("Revista", IPC1Proyecto1_201902259.tl2());
+        datos3.setValue("Libro Eléctronico", IPC1Proyecto1_201902259.tl3());
+        JFreeChart graficalb = ChartFactory.createPieChart3D("Tipos de Libros Registrados en el Sistema", datos3);
+        PiePlot3D pie2 = (PiePlot3D) graficalb.getPlot();
+        pie2.setForegroundAlpha(0.40f);
+        pie2.setInteriorGap(0.05);
+        pie2.setBackgroundPaint(Color.LIGHT_GRAY);
+        ChartPanel p3 = new ChartPanel(graficalb);
+        p3.setBounds(640, 50, 290, 290);
+        this.add(p3);
+
         //Diseño del Panel
         this.setLayout(null);
-        
-        
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
-    
+
 }
--- [IPC1] Proyecto 1_201902259/src/ipc1/proyecto/pkg1_201902259/IPC1Proyecto1_201902259.java
+++ [IPC1] Proyecto 1_201902259/src/ipc1/proyecto/pkg1_201902259/IPC1Proyecto1_201902259.java
@@ -1,10 +1,15 @@
-
 package ipc1.proyecto.pkg1_201902259;
+
 import Interfazg.login;
 import Clases.*;
 import Interfazg.PantallaPrincipal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class IPC1Proyecto1_201902259 {
+
     //Creación del arreglo de libros
     static Libro[] libros = new Libro[100];
     static int contlibros = 0;
@@ -17,53 +22,56 @@ public class IPC1Proyecto1_201902259 {
     //CReación de arreglo de reportes
     static Reporte[] reportes = new Reporte[100];
     static int contrep = 0;
-    
+
     public static void main(String[] args) {
-        
+
         login ventana = new login();
         //PantallaPrincipal p = new PantallaPrincipal();
-        
+
     }
+
     //Metodo para guardar el libro en el arreglo de libros
-    public static void crearlibro(Libro nuevo){
+    public static void crearlibro(Libro nuevo) {
         if (contlibros < libros.length) {
             libros[contlibros] = nuevo;
             contlibros++;
-            
+
         }
     }
-    
-    public static void verlibros(){
+
+    public static void verlibros() {
         for (int i = 0; i < contlibros; i++) {
             System.out.println("----------------------------------------------");
-            if (libros[i]!=null) {
+            if (libros[i] != null) {
                 System.out.println("ID: " + libros[i].getId());
                 System.out.println("Nombre del Libro: " + libros[i].getTitulo());
                 System.out.println("Autor: " + libros[i].getAutor());
-                System.out.println("Copias: "+ libros[i].getCopias());
+                System.out.println("Copias: " + libros[i].getCopias());
                 System.out.println("Tipo: " + libros[i].getTipo());
                 System.out.println("Disponibles: " + libros[i].getDisponibles());
                 System.out.println("Ocupados: " + libros[i].getOcupados());
-                
+
             }
-            
+
         }
     }
-    public static Libro obtenerlibro(int id){
+
+    public static Libro obtenerlibro(int id) {
         for (int i = 0; i < contlibros; i++) {
-            if (id==libros[i].getId()) {
+            if (id == libros[i].getId()) {
                 return libros[i];
             }
-        } return null;
-    } 
-    
-    public static Object[][] tablitalibros(){
+        }
+        return null;
+    }
+
+    public static Object[][] tablitalibros() {
         Object[][] contenido = new Object[contlibros][7];
         for (int i = 0; i < contlibros; i++) {
             contenido[i][0] = libros[i].getId();
             contenido[i][1] = libros[i].getTitulo();
             contenido[i][2] = libros[i].getAutor();
-            switch(libros[i].getTipo()){
+            switch (libros[i].getTipo()) {
                 case 1:
                     contenido[i][3] = "Libro";
                     break;
@@ -77,49 +85,47 @@ public class IPC1Proyecto1_201902259 {
             contenido[i][4] = libros[i].getCopias();
             contenido[i][5] = libros[i].getDisponibles();
             contenido[i][6] = libros[i].getOcupados();
-            
-            
-            
+
         }
         return contenido;
     }
-    
-    public static boolean validid(int id){
+
+    public static boolean validid(int id) {
         for (int i = 0; i < contlibros; i++) {
-            if (id==libros[i].getId()) {
+            if (id == libros[i].getId()) {
                 return true;
             }
         }
         return false;
     }
-    
-     //Metodo para guardar los usuarios en el arreglo de usuarios
-    public static void crearuser(Usuario nuevo){
+
+    //Metodo para guardar los usuarios en el arreglo de usuarios
+    public static void crearuser(Usuario nuevo) {
         if (contusers < usuarios.length) {
             usuarios[contusers] = nuevo;
             contusers++;
-            
+
         }
     }
-    
-    public static void verusuarios(){
+
+    public static void verusuarios() {
         for (int i = 0; i < contusers; i++) {
             System.out.println("----------------------------------------------");
-            if (usuarios[i]!=null) {
+            if (usuarios[i] != null) {
                 System.out.println("ID: " + usuarios[i].getId());
                 System.out.println("Usuario: " + usuarios[i].getUser());
                 System.out.println("Password: " + usuarios[i].getPassword());
-                System.out.println("Facultad: "+ usuarios[i].getFacultad());
+                System.out.println("Facultad: " + usuarios[i].getFacultad());
                 System.out.println("Carrera: " + usuarios[i].getCarrera());
                 System.out.println("Tipo: " + usuarios[i].getTipo());
-                               
-                
+
             }
-            
+
         }
     }
+
     //validando si el usuario existe 
-    public static boolean entrusuario(String us){
+    public static boolean entrusuario(String us) {
         for (int i = 0; i < contusers; i++) {
             if (us.equals(usuarios[i].getUser())) {
                 return true;
@@ -127,93 +133,380 @@ public class IPC1Proyecto1_201902259 {
         }
         return false;
     }//parte de la validación del usuario, como contraseña
-    public static boolean passusuario(String pas){
+
+    public static boolean passusuario(String pas) {
         for (int i = 0; i < contusers; i++) {
             if (pas.equals(usuarios[i].getPassword())) {
                 return true;
-                
+
             }
-            
+
         }
         return false;
     }
-    public static Usuario retornus(String us){
+
+    public static Usuario retornus(String us) {
         for (int i = 0; i < contusers; i++) {
             if (us.equals(usuarios[i].getUser())) {
                 return usuarios[i];
             }
-            
+
         }
         return null;
     }
-    
-     //Metodo para guardar los prestamos
-    public static void crearprestamo(Prestamo nuevo){
+
+    //Metodo para guardar los prestamos
+    public static void crearprestamo(Prestamo nuevo) {
         if (contpres < prestamos.length) {
             prestamos[contpres] = nuevo;
             contpres++;
-            
+
         }
     }
-    
-    public static void verprestamos(){
+
+    public static void verprestamos() {
         for (int i = 0; i < contpres; i++) {
             System.out.println("----------------------------------------------");
-            if (prestamos[i]!=null) {
+            if (prestamos[i] != null) {
                 System.out.println("ID Libro: " + prestamos[i].getIdlibro());
                 System.out.println("ID Usuario: " + prestamos[i].getIdestudiante());
                 System.out.println("Fecha: " + prestamos[i].getFecha());
-                System.out.println("Status: "+ prestamos[i].getStatus());
-                
-                               
-                
+                System.out.println("Status: " + prestamos[i].getStatus());
+
             }
-            
+
         }
     }
+
     //función para retornar el nombre del usuario por medio del Id
-    public static String nameus(int Id){
+    public static String nameus(int Id) {
         for (int i = 0; i < contusers; i++) {
             if (Id == usuarios[i].getId()) {
                 return usuarios[i].getUser();
             }
-        }return null;
+        }
+        return null;
     }
-       public static String namelib(int Id){
+
+    public static String namelib(int Id) {
         for (int i = 0; i < contlibros; i++) {
             if (Id == libros[i].getId()) {
                 return libros[i].getTitulo();
             }
-        }return null;
+        }
+        return null;
     }
+
     //Validaciones para verificar que exista el usuario   
-    public static boolean verfus(int Id){
+    public static boolean verfus(int Id) {
         for (int i = 0; i < contusers; i++) {
             if (Id == usuarios[i].getId()) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
+
     //Validaciones para verificar que exista el libro
-       public static boolean verlb(int Id){
+    public static boolean verlb(int Id) {
         for (int i = 0; i < contlibros; i++) {
             if (Id == libros[i].getId()) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
- 
-    
-    public static Object[][] tablitaprestamos(){
+
+    public static Object[][] tablitaprestamos() {
         Object[][] contenido = new Object[contpres][4];
         for (int i = 0; i < contpres; i++) {
             contenido[i][0] = nameus(prestamos[i].getIdestudiante()); //se llamó la funcion nameus para retornar el nombre del usuario por medio del ID
             contenido[i][1] = namelib(prestamos[i].getIdlibro()); //se llamó a namelib para obtener el titulo del libro
             contenido[i][2] = prestamos[i].getFecha();
             contenido[i][3] = prestamos[i].getStatus();
-                      
-                      
+
         }
         return contenido;
     }
+
+    public static int ust1() {
+        int contador = 0;
+        for (int i = 0; i < contusers; i++) {
+            if (usuarios[i].getTipo() == 1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int ust2() {
+        int contador = 0;
+        for (int i = 0; i < contusers; i++) {
+            if (usuarios[i].getTipo() == 2) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int tl1() {
+        int contador = 0;
+        for (int i = 0; i < contlibros; i++) {
+            if (libros[i].getTipo() == 1) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int tl2() {
+        int contador = 0;
+        for (int i = 0; i < contlibros; i++) {
+            if (libros[i].getTipo() == 2) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int tl3() {
+        int contador = 0;
+        for (int i = 0; i < contlibros; i++) {
+            if (libros[i].getTipo() == 3) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int enero() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("0")) {
+                    contador++;
+
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int febrero() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("1")) {
+                    contador++;
+
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int marzo() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("2")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int abril() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("3")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int mayo() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("4")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int junio() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("5")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int julio() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("6")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int agosto() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("7")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int septiembre() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("8")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int octubre() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("9")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int noviembre() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("10")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
+    public static int diciembre() {
+        int contador = 0;
+        for (int i = 0; i < contpres; i++) {
+            DateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+            try {
+                Date fecha = f.parse(prestamos[i].getFecha());
+                Calendar c = Calendar.getInstance();
+                c.setTime(fecha);
+                if (String.valueOf(c.get(Calendar.MONTH)).equals("11")) {
+                    contador++;
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+        return contador;
+    }
+
 }
